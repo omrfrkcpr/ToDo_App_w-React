c6173cfa1d9ab670b32fba541d9413fffc634671
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-03-04 23:48:37 +0100

    feature: clear all functionality (incl. local storage)

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -5,6 +5,9 @@ export function Tasks({ tasks, onDelete, onComplete, onEdit }) {
   const tasksQuantity = tasks.length;
   const completedTasks = tasks.filter((task) => task.isCompleted).length;
 
+  // clear local storage
+  const handleClear = () => localStorage.clear();
+
   return (
     <section className="tasks w-100">
       <header className="header d-flex align-items-center justify-content-between w-75 mx-auto bg-white">
@@ -31,6 +34,24 @@ export function Tasks({ tasks, onDelete, onComplete, onEdit }) {
             onEdit={onEdit}
           />
         ))}
+      </div>
+
+      <div className="clear mt-3 text-center">
+        <button
+          className="btn clear-btn border-2"
+          onClick={() => {
+            if (
+              window.confirm(
+                "Are you sure you want to clear your entire todo list permanently?"
+              )
+            ) {
+              handleClear();
+              window.location.reload();
+            }
+          }}
+        >
+          Clear
+        </button>
       </div>
     </section>
   );
